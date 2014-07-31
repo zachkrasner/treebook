dea3d52eb8bcad5edad9ad8c24b4d4170cbba1ca
Zach Krasner <zachkrasner@gmail.com>
2014-07-31 17:14:44 -0400

    Install user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes
 
   resources :statuses
+
+  root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731173439) do
+ActiveRecord::Schema.define(version: 20140731211102) do
 
   create_table "recipes", force: true do |t|
     t.string   "name"
@@ -23,8 +23,28 @@ ActiveRecord::Schema.define(version: 20140731173439) do
   end
 
   create_table "statuses", force: true do |t|
-    t.string   "name"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
