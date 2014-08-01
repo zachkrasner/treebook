d6665ca0f7b62ba2faa4be43c167d5c48ee2c6df
Zach Krasner <zachkrasner@gmail.com>
2014-08-01 02:51:53 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :recipes
-
   resources :statuses
+  get "feed", to: "statuses#index", as: :feed
 
   root to: 'statuses#index'
+
+  devise_scope :user do
+    get "register", :to => "devise/registrations#new", as: :register
+    get "login", :to => "devise/sessions#new", as: :login
+    get "logout", :to => "devise/sessions#destroy", as: :logout
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
