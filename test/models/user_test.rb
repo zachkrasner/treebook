5a00185386bff29e2227c27a35d1ed22b0657eb6
Zach Krasner <zachkrasner@gmail.com>
2014-08-01 16:44:51 -0400

    Fixed bug

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,9 +31,19 @@ class UserTest < ActiveSupport::TestCase
 
   test "a user should have a profile name without spaces" do
   	user = User.new
+    user.profile_name = "My profile with spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must not have spaces.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: "Jason", last_name: 'Seifer', email:'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'jasonseifer_1'
+    assert user.valid?
+  end
+
 end
