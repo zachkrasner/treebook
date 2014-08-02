e7bd8f93d3385c03a13826d3e31af5b4223b9f40
Zach Krasner <zachkrasner@gmail.com>
2014-08-01 20:49:24 -0400

    Fixed Route problem. Move devise routes up in file as they run up to down

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
